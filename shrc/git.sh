0ba4925fef0188f6229f096453d63f5aecd0b4e8
Levent Ali <lebreeze@gmail.com>
2013-09-17 08:41:20 -0400

    Add branch to git status alias sb

--- shrc/git.sh
+++ shrc/git.sh
@@ -4,7 +4,7 @@ alias gd='git diff'
 alias gc='git commit -v'
 alias gca='git commit -v -a'
 alias gb='git branch -v'
-alias st='git status -s'
+alias st='git status -sb'
 alias gs='git log --oneline --decorate -10 --color'
 
 function gco {
