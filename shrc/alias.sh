1a0c7ae70440ac82479da9ceef1c4b62c7b630e8
Levent Ali <lebreeze@gmail.com>
2014-04-30 07:56:44 +0000

    nfind

--- shrc/alias.sh
+++ shrc/alias.sh
@@ -6,4 +6,7 @@ alias v='vim -p'
 alias p='ps waxu'
 alias s='python -m SimpleHTTPServer'
 
+function nfind { find . -name "*$1*"; }
+export -f nfind
+
 alias tinfo='echo "SESSIONS" ; tmux list-sessions ; echo "WINDOWS" ; tmux list-windows ; echo "PANES" ; tmux list-panes'
