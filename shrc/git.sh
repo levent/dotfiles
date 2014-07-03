dc0be4ef29dba20b4d386640031af2563a6f15d8
Levent Ali <lebreeze@gmail.com>
2014-07-03 10:05:32 +0000

    alias gf and grp

--- shrc/git.sh
+++ shrc/git.sh
@@ -7,6 +7,8 @@ alias gca='git commit -v -a'
 alias gb='git branch -v'
 alias st='git status -sb'
 alias gs='git log --oneline --decorate -10 --color'
+alias gf='git fetch'
+alias grp='git remote prune'
 
 function gco {
   if [ $# -eq 0 ]; then
